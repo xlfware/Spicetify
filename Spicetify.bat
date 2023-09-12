a9841fb8b5522a8bf27670e8cc4dcfbb3a06fbea
xlf <125935975+xlfware@users.noreply.github.com>
2023-09-11 21:18:03 -0700

    Add files via upload

---
+++ Spicetify.bat
@@ -0,0 +1,172 @@
+@echo off && title spicetify && color 03
+::R.1.5
+
+Echo Waiting for cmd to fully load
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo ◾ ⬛ ∎ 🔲 🔳 ⊞ ⊟ ⊠ ⊡ ❏ ❐ ❑ ❒ ▀ ◧ ◨ ◩ ◪ ▢ ▣ ▤ ▥ ▦ ▧ ▨ ▩ ▁ ▂ ▃ ▄ ▅ ▆ ▆ ▇
+Echo Finished loading
+:start
+cls
+
+:::           
+:::                                   _____       _          _   _  __                                                   
+:::                                  / ____|     (_)        | | (_)/ _|                                                  
+:::                                 | (___  _ __  _  ___ ___| |_ _| |_ _   _                                             
+:::                                  \___ \| '_ \| |/ __/ _ | __| |  _| | | |                                            
+:::                                  ____) | |_) | | (_|  __| |_| | | | |_| |                                            
+:::                                 |_____/| .__/|_|\___\___|\__|_|_|  \__, |                                            
+:::                                        | |                          __/ |                                            
+:::    _____             _   _  __         |_|   _____  _       _ _    |____|  ______ _                       
+:::   / ____|           | | (_)/ _|     ( )     |  __ \(_)     (_| |      | | |  ____| |                      
+:::  | (___  _ __   ___ | |_ _| |_ _   _|/ ___  | |  | |_  __ _ _| |_ __ _| | | |__  | | __ ___   _____  _ __ 
+:::   \___ \| '_ \ / _ \| __| |  _| | | | / __| | |  | | |/ _` | | __/ _` | | |  __| | |/ _` \ \ / / _ \| '__|
+:::   ____) | |_) | (_) | |_| | | | |_| | \__ \ | |__| | | (_| | | || (_| | | | |    | | (_| |\ V | (_) | |   
+:::  |_____/| .__/ \___/ \__|_|_|  \__, | |___/ |_____/|_|\__, |_|\__\__,_|_| |_|    |_|\__,_| \_/ \___/|_|   
+:::         | |                     __/ |                  __/ |                                              
+:::         |_|                    |___/                  |___/                                            
+
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+
+timeout /t 3 >nul
+
+echo ___________________________________________________________________________________________________________
+@ping -n 1 -w 1000 0.0.0.1 > NUL  
+echo ^| 1 ^| Install Spicetify    ^| Installs Spicetify                                                           ^|
+@ping -n 1 -w 1000 0.0.0.1 > NUL  
+echo ^|_________________________________________________________________________________________________________^|
+@ping -n 1 -w 1000 0.0.0.1 > NUL  
+echo ^| 2 ^| Uninstall Spicetify  ^| Uninstalls Spicetify                                                         ^|
+@ping -n 1 -w 1000 0.0.0.1 > NUL  
+echo ^|_________________________________________________________________________________________________________^|
+@ping -n 1 -w 1000 0.0.0.1 > NUL  
+echo ^| 3 ^| Install Spotify      ^| Installs the correct Spotify                                                 ^|
+@ping -n 1 -w 1000 0.0.0.1 > NUL  
+echo ^|_________________________________________________________________________________________________________^|
+@ping -n 1 -w 1000 0.0.0.1 > NUL  
+echo ^| 4 ^| Uninstall Spotify    ^| Uninstalls Windows Spotify (The wrong one)                                   ^|
+@ping -n 1 -w 1000 0.0.0.1 > NUL  
+echo ^|_________________________________________________________________________________________________________^|
+
+
+set /p SY=Type option:
+if "%SY%"=="1" goto InstallS
+if "%SY%"=="2" goto UninstallS
+if "%SY%"=="3" goto InstallRS
+if "%SY%"=="4" goto UninstallWS
+
+exit
+
+:InstallS
+cls
+set Spicy=D#k9f@Pz3mQ7wXl1S2vU6nYt8H5cGJiV.ps1
+(
+  echo iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 ^| iex
+  echo #
+  echo iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 ^| iex
+  echo #
+  echo cls
+  echo #
+  echo Write-Host "Cleaning Up!"
+  echo #
+  echo Write-Host "Close Window Whenever You're Ready!"
+  echo #
+  echo Remove-Item -Path $MyInvocation.MyCommand.Source ^| iex
+) > %Spicy%
+echo File "%Spicy%" created with the following content:
+type %Spicy%
+cls
+timeout /t 2 /nobreak >nul
+powershell.exe -ExecutionPolicy Bypass -File D#k9f@Pz3mQ7wXl1S2vU6nYt8H5cGJiV.ps1
+exit
+
+:UninstallS
+cls
+set Spicy=W5v#RcA9zP2k@EhT4mY1nL7gJ3iQ8oXs.ps1
+(
+  echo spicetify restore
+  echo #
+  echo rmdir -r -fo $env:APPDATA\spicetify
+  echo #
+  echo rmdir -r -fo $env:LOCALAPPDATA\spicetify
+  echo #
+  echo cls
+  echo #
+  echo Write-Host "Cleaning Up!"
+  echo #
+  echo Write-Host "Close Window Whenever You're Ready!"
+  echo #
+  echo Remove-Item -Path $MyInvocation.MyCommand.Source ^| iex
+) > %Spicy%
+echo File "%Spicy%" created with the following content:
+type %Spicy%
+cls
+timeout /t 2 /nobreak >nul
+powershell.exe -ExecutionPolicy Bypass -File W5v#RcA9zP2k@EhT4mY1nL7gJ3iQ8oXs.ps1
+exit
+
+:UninstallWS
+cls
+winget uninstall --id 9NCBCSZSJRSB
+cls
+color 03
+goto start
+exit
+
+:installRS
+cls
+echo Downloading Spotify...
+
+set URL=https://download.scdn.co/SpotifySetup.exe
+
+set LocalFilePath=SpotifySetup.exe
+
+curl -o %LocalFilePath% %URL%
+
+if exist %LocalFilePath% (
+    echo Download completed successfully.
+
+    start "" %LocalFilePath%
+
+    cls
+    echo Cleaning up
+    timeout /t 15 /nobreak
+
+    del %LocalFilePath%
+	cls
+    echo Finished cleaning
+	timeout /t 2 /nobreak
+	goto start
+) else (
+    echo Download failed.
+)
+
+exit
+
+
+
+
+
+
+
+
+
+
+REM Add feature to install file that installs the .ps1 script then deletes it after it's over
